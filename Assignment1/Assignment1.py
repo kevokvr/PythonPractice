25588e48c8d3d98951a34a9ee5916e5c0d8e20ad
kevokvr <kvalenzu@msudenver.edu>
2018-09-17 19:13:50 -0600

    linked list

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -1,18 +1,8 @@
 from __future__ import print_function
 import unittest
-# Kevin Valenzuela
-
-''' when run with "-m unittest", the following produces:
-    FAILED (failures=9, errors=2)
-    your task is to fix the failing tests by implementing the necessary
-    methods. '''
-
 
 class LinkedList(object):
     class Node(object):
-        # pylint: disable=too-few-public-methods
-        ''' no need for get or set, we only access the values inside the
-            LinkedList class. and really: never have setters. '''
 
         def __init__(self, value, next_node):
             self.value = value
@@ -23,7 +13,7 @@ class LinkedList(object):
         if initial is not None:
             for i in initial:
                 self.push_front(i)
-    # Kevin Valenzuela
+
     def empty(self):
         return self.front == self.back == None
 
@@ -44,7 +34,6 @@ class LinkedList(object):
         self.reverse()
         string = ', '.join(map(str, self))
         return string
-    # Had to research the str method, credit to.  https://stackoverflow.com/questions/727761/python-str-and-lists
 
     def __repr__(self):
         return 'LinkedList((' + self.__str__()+ '))'
@@ -59,16 +48,12 @@ class LinkedList(object):
             front = next
         self.front = previous
 
-
-
     def push_front(self, value):
         new = self.Node(value, self.front)
         if self.empty():
             self.front = self.back = new
         else:
             self.front = new
-
-    ''' you need to(at least) implement the following three methods'''
 
     def pop_front(self):
         if self.empty():
@@ -114,21 +99,16 @@ class LinkedList(object):
                 last = last.next_node
             previous.next_node = None
             self.back = previous
+            self.back = previous
         return value
 
-
-
-
     # Used this for testing
-    # def printLinkedList(self):
-    #     node = self.front
-    #     print("The List.")
-    #     while node:
-    #         print(node.value)
-    #         node = node.next_node
-
-# ''' C-level work '''
-
+    def printLinkedList(self):
+        node = self.front
+        print("The List.")
+        while node:
+            print(node.value)
+            node = node.next_node
 
 class TestEmpty(unittest.TestCase):
     def test(self):
@@ -175,7 +155,7 @@ class TestPushBackPopFront(unittest.TestCase):
         self.assertEqual(linked_list.pop_front(), 3)
         self.assertTrue(linked_list.empty())
 
-
+		
 class TestPushBackPopBack(unittest.TestCase):
     def test(self):
         linked_list = LinkedList()
@@ -188,9 +168,6 @@ class TestPushBackPopBack(unittest.TestCase):
         self.assertEqual(linked_list.pop_back(), "foo")
         self.assertEqual(linked_list.pop_back(), 1)
         self.assertTrue(linked_list.empty())
-
-
-# ''' B-level work '''
 
 
 class TestInitialization(unittest.TestCase):
@@ -208,9 +185,6 @@ class TestStr(unittest.TestCase):
         self.assertEqual(linked_list.__str__(), '1, 2, 3')
 
 
-# ''' A-level work '''
-
-
 class TestRepr(unittest.TestCase):
     def test(self):
         linked_list = LinkedList((1, 2, 3))
@@ -223,55 +197,6 @@ class TestErrors(unittest.TestCase):
 
     def test_pop_back_empty(self):
         self.assertRaises(RuntimeError, lambda: LinkedList().pop_back())
-#
-#     KEVIN VALENZUELA
-# ''' write some more test cases. '''
-#
-# ''' extra credit.
-#     - write test cases for and implement a delete(value) method.
-#     - write test cases for and implement a method that finds the middle
-#       element with only a single traversal.
-# '''
-#
-# ''' the following is a demonstration that uses our data structure as a
-#     stack'''
-#
-#
-# def fact(number):
-#     '''"Pretend" to do recursion via a stack and iteration'''
-#
-#     if number < 0:
-#         raise ValueError("Less than zero")
-#     if number == 0 or number == 1:
-#         return 1
-#
-#     stack = LinkedList()
-#     while number > 1:
-#         stack.push_front(number)
-#         number -= 1
-#
-#     result = 1
-#     while not stack.empty():
-#         result *= stack.pop_front()
-#
-#     return result
-#
-#
-# class TestFactorial(unittest.TestCase):
-#     def test_less_than_zero(self):
-#         self.assertRaises(ValueError, lambda: fact(-1))
-#
-#     def test_zero(self):
-#         self.assertEqual(fact(0), 1)
-#
-#     def test_one(self):
-#         self.assertEqual(fact(1), 1)
-#
-#     def test_two(self):
-#         self.assertEqual(fact(2), 2)
-#
-#     def test_10(self):
-#         self.assertEqual(fact(10), 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
 
 
 if '__main__' == __name__:
